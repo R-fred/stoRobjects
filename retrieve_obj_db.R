56b2b895767b2c63177f1851838b6de43f35674b
fred <fchesneau@gmail.com>
2019-03-03 10:43:00 +0000

    Created two files to contain the two basic operations: save and retrieve. Small description of what should be done has been added to each file.

---
+++ retrieve_obj_db.R
@@ -0,0 +1,8 @@
+#### Retrieve object from database ####
+
+# Need to:
+#         1. List information about all objects.
+#         2. Select the object based on the information retrieved
+#         3. Get the object
+#         4. Unserialise the object into a new object (possibly with the same name?)
+#         5. Keep a record of how many times the object has been used
---
+++ save_obj_db.R
@@ -0,0 +1,9 @@
+#### Save object in database ####
+
+# Need to:
+#         1. Get information about the object
+#         2. Get the object and serialise it
+#         3. Insert the inforamtion about the object in the database
+#         4. Insert the serialised object into the database
+#         5. Create/recreate index on object information
+
