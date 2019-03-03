397aba7d09466948fc751fb291c0fdfa35d7bdeb
fred <fchesneau@gmail.com>
2019-03-03 16:15:07 +0000

    created functions to list objects and get basic information about them.

--- save_obj_db.R
+++ save_obj_db.R
@@ -7,3 +7,35 @@
 #         4. Insert the serialised object into the database
 #         5. Create/recreate index on object information
 
+list_objects <- function(envir = globalenv()){
+
+  output <- objects(name = envir, sorted = T)
+
+  return(list(output, TRUE))
+
+}
+
+get_object_info <- function(object){
+
+  read_obj <- function(object){
+
+    obj <- eval(parse(text = object))
+
+    list(name = as.character(object),
+         class = class(obj),
+         str = capture.output(str(obj)),
+         length = ifelse(length(grep(x = class(obj), pattern = "data\\.frame|data\\.table|tibble")), nrow(obj), length(obj)),
+         n_colums = ifelse(length(grep(x = class(obj), pattern = "data\\.frame|data\\.table|tibble")), ncol(obj), NA)
+        )
+  }
+
+  output <- lapply(X = object, FUN = function(x) read_obj(x))
+  names(output) <- object
+
+  return(output)
+
+}
+
+serialize_object <- function(object_name){
+
+}
